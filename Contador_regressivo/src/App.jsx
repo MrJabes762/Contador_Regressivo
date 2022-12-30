290b4ae1aa88b936e6b315cbca86ee13a732e4f8
MrJabes762 <118413268+MrJabes762@users.noreply.github.com>
2022-12-30 00:12:37 -0300

    Adicionando Imagem de fundo e textos de exibição

--- Contador_regressivo/src/App.jsx
+++ Contador_regressivo/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Title from "./components/Title";
+import Counter from "./components/Counter";
+import Ano_novo from "./assets/Ano_novo.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" style={{backgroundImage: `url(${Ano_novo})`}}>
+      <div className='Container'>
+        <Title title="Comtagem regressiva para 2023" />
+        <div className="countdow-container">
+          <Counter title = "Dias" number = {2} />
+          <Counter title = "Horas" number = {2} />
+          <Counter title = "Minutos" number = {2} />
+          <Counter title = "Segundos" number = {2} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
